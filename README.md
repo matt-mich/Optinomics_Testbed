122cb937023be4e33e01f44a72132f516c7bff1f
Matthew Michaelis <matthew.michaelis@ucdenver.edu>
2019-04-21 14:23:52 -0600

    Init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Optinomics_Testbed
+
+Don't use anything here in any capacity unless you want a broken computer. If you do want that, there are probably better ways to do it.
---
+++ update.sh
@@ -0,0 +1,4 @@
+cp ./lightdm-opti-greeter.desktop /usr/share/xgreeters/
+cp ./opti_greeter.py /usr/share/xgreeters/
+cp ./lightdm-opti-greeter.conf /etc/lightdm/
+cp ./lightdm.conf /etc/lightdm/
