13170772d54dd8136cc9b308d961c1cdc6164ba8
Matthew Michaelis <matthew.michaelis@ucdenver.edu>
2019-04-21 15:13:28 -0600

    Update

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 cp ./lightdm-opti-greeter.desktop /usr/share/xgreeters/
-cp ./opti_greeter.py /usr/share/xgreeters/
+cp ./opti_greeter.py /usr/local/bin/
 cp ./lightdm-opti-greeter.conf /etc/lightdm/
 cp ./lightdm.conf /etc/lightdm/
 cp ./opti_greeter.ui /etc/lightdm/
