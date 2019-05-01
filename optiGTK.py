f6c2d1c827c968252b5f974337ee84a8a78318b8
Matthew Michaelis <matthew.michaelis@ucdenver.edu>
2019-05-01 17:00:22 -0600

    Update optiGTK.py

--- optiGTK.py
+++ optiGTK.py
@@ -30,13 +30,6 @@ if __name__ == "__main__":
     class OptiGreeter(Gtk.Window):
         def __init__(self):
             Gtk.Window.__init__(self, title="OptiGreeter")
-            vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
-            self.add(vbox)
-            #self.u_entry = Gtk.Entry()
-
-            #self.p_entry = Gtk.Entry()
-            self.button = Gtk.Button(label="Click Here")
-            vbox.pack_start(self.button, True, True, 0)
 
             #self.add(self.u_entry)
             #self.add(self.p_entry)
