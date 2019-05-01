a1376106ef5673711a34045c5a35d5e56d5b3a3b
Matthew Michaelis <matthew.michaelis@ucdenver.edu>
2019-05-01 16:48:09 -0600

    aaaaa

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,6 +1,13 @@
-    if not DEV:
-    	greeter.connect_to_daemon_sync()
+        def __init__(self):
+            Gtk.Window.__init__(self, title="OptiGreeter")
+            vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
+            self.add(vbox)
+            #self.u_entry = Gtk.Entry()
 
-    window.show_all()
 
-    Gtk.main()
+            #self.p_entry = Gtk.Entry()
+            self.button = Gtk.Button(label="Click Here")
+            vbox.pack_start(self.button, True, True, 0)
+
+            #self.add(self.u_entry)
+            #self.add(self.p_entry)
