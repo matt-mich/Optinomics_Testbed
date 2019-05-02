a62500ff0e50d83ab2ac2e199207d38dbe48034d
Matthew Michaelis <matthew.michaelis@ucdenver.edu>
2019-05-02 13:43:55 -0600

    Update optiGTK.py

--- optiGTK.py
+++ optiGTK.py
@@ -308,16 +308,16 @@ if __name__ == "__main__":
 
     css_P = Gtk.CssProvider()
     if DEV:
-        css_P.load_from_path("res/style.css")
-        Gtk.StyleContext.add_provider_for_screen(
-            Gdk.Screen.get_default(),
-            css_P,
-            400
-        )
+        # css_P.load_from_path("res/style.css")
+        # Gtk.StyleContext.add_provider_for_screen(
+        #     Gdk.Screen.get_default(),
+        #     css_P,
+        #     400
+        # )
         builder.add_from_file("gtk_glade.glade")
     else:
-        css_P.load_from_path("/usr/local/bin/optinomics/res/style.css")
-        Gtk.StyleContext.add_provider_for_screen(Gdk.Screen.get_default(),css_P,400)
+        #css_P.load_from_path("/usr/local/bin/optinomics/res/style.css")
+        #Gtk.StyleContext.add_provider_for_screen(Gdk.Screen.get_default(),css_P,400)
         builder.add_from_file("/usr/local/bin/optinomics/gtk_glade.glade")
 
     builder.connect_signals(Handler())
