35b7e5119fa8537594b14f947077b96502cc5e8a
Matthew Michaelis <matthew.michaelis@ucdenver.edu>
2019-05-02 12:54:11 -0600

    a

--- update.sh
+++ update.sh
@@ -1,8 +1,7 @@
 cp ./lightdm-opti-greeter.desktop /usr/share/xgreeters/
+cp ./lightdm-opti-greeter.conf /etc/lightdm/
+cp ./lightdm.conf /etc/lightdm/
 cp ./glade_py_test.py /usr/local/bin/optinomics/
 cp ./optiGTK.py /usr/local/bin/optinomics/
 cp ./gtk_glade.glade /usr/local/bin/optinomics/
 cp -R ./res /usr/local/bin/optinomics/
-cp ./lightdm-opti-greeter.conf /etc/lightdm/
-cp ./lightdm.conf /etc/lightdm/
-cp ./opti_greeter.ui /etc/lightdm/
